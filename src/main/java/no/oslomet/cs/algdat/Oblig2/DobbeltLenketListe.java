5c6d7b52a0f8a08addbbfcfae0948497ed763617
Diba00 <dibas.shishegar@gmail.com>
2022-09-29 15:47:26 +0200

    prøver å fikse gradlew

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -213,6 +213,12 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         throw new UnsupportedOperationException();
     }
 
+    public static void main(String[] args) {
+        String[] s = {"Ole", null, "Per", "Kari", null};
+        Liste<String> liste = new DobbeltLenketListe<>(s);
+        System.out.println(liste.antall() + " " + liste.tom());
+    }
+
 } // class DobbeltLenketListe
 
 
