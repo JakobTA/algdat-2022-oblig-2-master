b3ac2a87edb21f9d8e7d27b365053fefe873e71e
Ali Imran Anjum <ali.skole@outlook.com>
2022-10-02 17:58:49 +0200

    Oppgave 2: Har kodet main for å teste om lenketliste fungerer. Det fungerer. Nå skal vi jobbe med å lage omvendt toString.

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -67,11 +67,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
                 hale.neste = null;
                 hode.forrige = null;
             }
-
-
         }
-
-
         //throw new UnsupportedOperationException();
     }
 
@@ -211,6 +207,12 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     public static <T> void sorter(Liste<T> liste, Comparator<? super T> c) {
         throw new UnsupportedOperationException();
+    }
+
+    public static void main(String[] args) {
+        String[] s = {"Ole", null, "Per", "Kari", null};
+        Liste<String> liste = new DobbeltLenketListe<>(s);
+        System.out.println(liste);
     }
 
 } // class DobbeltLenketListe
