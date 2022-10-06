f83380d998993e9427d0b8f3517f3d77145b7f41
Jakob Anderson <jakob.anderson.work@gmail.com>
2022-10-06 17:45:01 +0200

    Oppgave 9 - Versjon 1
    Sliter med nullpointerexception på fjerning i midten. Kanskje burde vi lage en node som er denne.forrige med en gang metoden kalles? Da trenger man ikke  hele tiden å holde styr på at det er den til venstre som skal fjernes.

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -520,7 +520,29 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
         @Override
         public void remove() {
-            throw new UnsupportedOperationException();
+            //Først sjekker vi om vi har lov til å kalle på metoden nå
+            if(!fjernOK)
+                throw new IllegalStateException("Metoden kan ikke gjennomføres akkurat nå!");
+            //Så sjekker vi om endringer og iteratorendringer stemmer/er like
+            if(endringer != iteratorendringer)
+                throw new ConcurrentModificationException("Endringer og iteratorendringer er ikke like!");
+            fjernOK = false;
+
+            if(antall == 1){ //Tilfelle 1 - Vi har bare én node
+                hode = hale = null;
+            } else if(denne == null){ //Tilfelle 2 - Siste node skal fjernes
+                hale = hale.forrige; hale.neste = null;
+            } else if(denne == hode){ //Tilfelle 3 - Første node skal fjernes og det er flere enn én node i listen
+                hode = hode.neste; hode.forrige = null;
+            } else{ //Tilfelle 4 - Vi skal fjerne en node midt i listen (Til venstre for noden i midten)
+                denne = denne.forrige;
+                denne.neste.forrige = denne.forrige;
+                denne.forrige.neste = denne.neste;
+            }
+
+            //Oppdaterer til slutt endringer
+            antall--; endringer++; iteratorendringer++;
+            // throw new UnsupportedOperationException();
         }
 
     } // class DobbeltLenketListeIterator
